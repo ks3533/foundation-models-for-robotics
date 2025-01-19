9861bb3704b11aa08e7ca328b2197a242a5a44b1
Marika Prolingheuer <lo6375@partner.kit.edu>
2025-01-19 18:13:34 +0100

    fix the "violent shaking" turning the robot to grap from the side implement rotate_z (doesn't work consistently yet)

--- Code/main.py
+++ Code/main.py
@@ -81,8 +81,7 @@ class Controller:
         print("done")
 
     def rotate_gripper_abs(self, end_rotation: Sequence[int]) -> None:
-        """Rotates the gripper to an absolute end rotation
-        currently, if the rotation exceeds the maximum for the joints, the robot shakes violently"""
+        """Rotates the gripper to an absolute end rotation"""
         # TODO make execution more consistent
         # if end_rotation is quaternion, convert it to euler
         if len(end_rotation) == 4:
@@ -99,18 +98,66 @@ class Controller:
             angle_velocities = []
             for rotation in vector:
                 angle_velocity = 0
-                if abs(rotation) > 2:
+                if abs(rotation) > 5:
                     angle_velocity = self.max_angle_velocity
-                elif abs(rotation) > 0.2:
+                elif abs(rotation) > 1:
                     angle_velocity = self.min_angle_velocity
                 if rotation < 0:
                     angle_velocity = -angle_velocity
                 angle_velocities.append(angle_velocity)
 
             self.movement[3:6] = angle_velocities
+
+            #break if the angle difference is nearly zero for all three axes
+            if all(v == 0 for v in angle_velocities):
+                break
+
         self.movement = np.zeros(7)
         print("done")
 
+    def rotate_z(self, goal_rotation):
+        """rotates the gripper around the z-axis to the next angle at which it can grip the milk properly (i.e. the grippers are parallel to the sides of the carton)"""
+        """is interrupted when the distance between the target and current angle increases
+        it still happens frequently that the robot is turning in the wrong direction and i cant figure out why"""
+        last_diff = None
+        
+        possible_rotations = [
+        goal_rotation,
+        goal_rotation + 90,
+        goal_rotation - 90,
+        goal_rotation + 180,
+        goal_rotation - 180
+    ]
+        end_rotation = min(possible_rotations, key=lambda r: abs(quat_to_euler(controller.env.observation_spec()["robot0_eef_quat"])[2] - r))
+        
+        while True:
+            current_diff = quat_to_euler(controller.env.observation_spec()["robot0_eef_quat"])[2] - end_rotation
+            
+            if abs(current_diff) <= 1: break
+            
+            if abs(current_diff) > 5:
+                angle_velocity = self.max_angle_velocity
+            elif abs(current_diff) > 1:
+                angle_velocity = self.min_angle_velocity
+            else:
+                angle_velocity = 0  
+                break
+            
+            if current_diff < 0:
+                angle_velocity = -angle_velocity
+
+            if last_diff is not None and abs(last_diff) < abs(current_diff):
+                print("moving wrong direction")
+                break
+                
+            last_diff = current_diff
+            self.movement[5] = angle_velocity
+            print(f"goal: {end_rotation}")
+            print(f"current_diff: {current_diff} v: {angle_velocity}")
+            print(quat_to_euler(controller.env.observation_spec()["robot0_eef_quat"]))
+
+        self.movement = np.zeros(7)
+        print("done")
 
 def quat_to_euler(quat: Sequence[int]) -> numpy.ndarray:
     """Takes a quaternion and returns it as an euler angle"""
@@ -127,10 +174,12 @@ if __name__ == "__main__":
     controller.start()
 
     controller.open_gripper()
+    controller.rotate_gripper_abs([90, 90, 0])
     controller.move(*(controller.env.observation_spec()["Milk_pos"] + [0, 0, 0.1]))
+    controller.rotate_z((quat_to_euler(controller.env.observation_spec()["Milk_quat"])[2]))
     # TODO rotate that the milk doesn't fall back down
-    controller.rotate_gripper_abs([180, 0, quat_to_euler(controller.env.observation_spec()["Milk_quat"])[2] % 90])
-    controller.move(*(controller.env.observation_spec()["Milk_pos"] + [0, 0, 0.1]))
+    #controller.rotate_gripper_abs([180, 0, quat_to_euler(controller.env.observation_spec()["Milk_quat"])[2] % 90])
+    #controller.move(*(controller.env.observation_spec()["Milk_pos"] + [0, 0, 0.1]))
     controller.move(*(controller.env.observation_spec()["Milk_pos"] + [0, 0, 0.03]))
     controller.close_gripper()
     controller.move(*(controller.env.observation_spec()["robot0_eef_pos"] + [0, 0, 0.1]))
